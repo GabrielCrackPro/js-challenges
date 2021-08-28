2e3500b22234f46fc133e9298bd1fdcfd048178c
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-08-28 12:50:56 +0200

    Challenge Added 💻

---
+++ wordsToSentence.js
@@ -0,0 +1,7 @@
+// Write function which will create a string from a list of strings, separated by space.
+
+const wordsToSentence = (arr) => {
+return arr.join(' ')
+}
+
+console.log("['Hello','World'] =>",wordsToSentence(['Hello','World']))
